bb960920cff15ec9a66fe86b333fe2cc89c6fad5
GouthamGuna <ggowthamsankar@gmail.com>
2023-01-12 18:56:11 +0530

    spring-gateway-security first commit

---
+++ spring-gateway-security/src/main/java/in/gmsk/SpringGatewaySecurityApplication.java
@@ -0,0 +1,13 @@
+package in.gmsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewaySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGatewaySecurityApplication.class, args);
+	}
+
+}
