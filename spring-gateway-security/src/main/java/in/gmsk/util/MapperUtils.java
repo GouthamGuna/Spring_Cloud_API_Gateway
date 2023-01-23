3a08eefbf2982e0781c9d3ed8fda9246596b25f1
GouthamGuna <ggowthamsankar@gmail.com>
2023-01-23 18:01:19 +0530

    Redis utils mapper

--- spring-gateway-security/src/main/java/in/gmsk/util/MapperUtils.java
+++ spring-gateway-security/src/main/java/in/gmsk/util/MapperUtils.java
@@ -1,2 +1,13 @@
-package in.gmsk.util;public class MapperUtils {
+package in.gmsk.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MapperUtils {
+    ObjectMapper mapper = new ObjectMapper();;
+
+    public  <T> T objectMapper(Object object,Class<T> contentClassType){
+        return mapper.convertValue(object, contentClassType);
+    }
 }
